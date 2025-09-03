7838dc2881beb846279937b2cf756c0e653e9483
Mujeeb <lawalsakamujeeb@gmail.com>
2025-09-03 15:20:06 -0400

    Code edited some codes

--- ec2_manager.py
+++ ec2_manager.py
@@ -3,24 +3,24 @@ import boto3
 client = boto3.client('ec2')
 
 def list_instance():
-    response = ec2.describe_instance()
+    response = client.describe_instances()
     print("EC2 instance(s) created:")
-    for reservation in response ["Reservations:"]:
-            for instance in reservation ["Instances"]:
-                  print(f" -ID:{instance['instanceId']}, State: {instance['State'],['Name']}")
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            print(f" -ID: {instance['InstanceId']}, State: {instance['State']['Name']}")
 
 def start_instance(instance_id):
-      try:
-            ec2.start_instance(InstanceIds=(instance_id))
-            print(f"Instance {instance_id} started successfully")
-      except Exception as e:
-            print(f"Error while starting: {e}")
+    try:
+        client.start_instances(InstanceIds=[instance_id])
+        print(f"Instance {instance_id} started successfully")
+    except Exception as e:
+        print(f"Error while starting: {e}")
 
 def stop_instance(instance_id):
-      try:
-            ec2.stop_instance(InstanceIds=(instance_id))
-            print(f"Instance {instance_id} successfully stopped")
-      except Exception as e:
-            print (f"Error while stopping instance: {e}")
-            
+    try:
+        client.stop_instances(InstanceIds=[instance_id])
+        print(f"Instance {instance_id} successfully stopped")
+    except Exception as e:
+        print(f"Error while stopping instance: {e}")
+
 
