f36321e4ced257ebde02ad80177e79759d572334
Mujeeb <lawalsakamujeeb@gmail.com>
2025-09-03 15:21:33 -0400

    Fix S3 client reference in list_buckets and create_bucket functions

--- s3_manager.py
+++ s3_manager.py
@@ -3,14 +3,14 @@ import boto3
 client = boto3.client('s3')
 
 def list_buckets():
-    response = s3.list_buckets()
+    response = client.list_buckets()
     print("S3 Buckets: ")
     for bucket in response.get("Buckets", []):
         print(f" - {bucket ['Name']}")
 
 def create_bucket(bucket_name):
     try:
-        s3.create_bucket(Bucket = bucket_name)
+        client.create_bucket(Bucket = bucket_name)
         print(f'{bucket_name}' "has been created successfully")
 
     except Exception as e:
